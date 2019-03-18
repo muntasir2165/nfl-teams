946d3651a47649e041a577986624301f1b1929a6
Muntasir Biojid <muntasirbiojid@Muntasirs-MacBook-Pro.local>
2019-03-17 21:26:38 -0700

    implemented team update via react componentDidUpdate lifecycle method

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import TeamInfo from "./data.json";
 import TeamListing from "./components/TeamListing";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      teams: TeamInfo.teams
+    });
+  }
+
   render() {
-    console.log(TeamInfo.teams);
+    // console.log(TeamInfo.teams);
+    console.log(this.state.teams);
     return (
       <div className="App">
         <h1>NFL Teams</h1>
-        <TeamListing teams={TeamInfo.teams}/>
+        <TeamListing teams={this.state.teams}/>
       </div>
     );
   }
--- src/components/TeamListing/TeamListing.js
+++ src/components/TeamListing/TeamListing.js
@@ -3,7 +3,7 @@ import "./TeamListing.css";
 
 const TeamListing = (props) => (
   <div>
-    {props.teams.map(team => <div key={team.id}>{team.fullName} {team.nickName} {team.roster.length}</div>)}
+      {props.teams ? props.teams.map(team => (<div key={team.id}>{team.fullName} {team.nickName} {team.roster.length}</div>)) : ""}
   </div>
 );
 
